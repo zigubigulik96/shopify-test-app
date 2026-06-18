42e1f8ad2a735e274f8e07cbc686a41593cedd13
zigubigulik96 <david.svestak96@gmail.com>
2026-06-18 21:48:17 +0200

    Update start.js

--- functions/auth/start.js
+++ functions/auth/start.js
@@ -7,7 +7,7 @@ export async function onRequestGet({ request, env }) {
   }
 
   // Minimal scopes for testing
-  const scope = "read_metaobject_definitions,write_metaobject_definitions,read_metaobjects,write_metaobjects,read_orders,read_product_feeds,write_product_feeds,read_product_listings,write_product_listings,read_products,write_products,customer_read_metaobjects,unauthenticated_read_metaobjects";
+  const scope = "read_all_orders,read_assigned_fulfillment_orders,write_assigned_fulfillment_orders,read_companies,write_companies,read_customers,write_customers,write_locations,read_locations,read_metaobject_definitions,write_metaobject_definitions,read_metaobjects,write_metaobjects,write_order_edits,read_order_edits,read_orders,write_orders,read_product_feeds,write_product_feeds,read_product_listings,write_product_listings,read_products,write_products,customer_read_draft_orders,customer_read_orders,customer_write_orders";
 
   const redirectUri = encodeURIComponent(`${url.origin}/auth/callback`);
   const installUrl =
